39d30660c7dab45deb71c6c0b0e8007d5cc00141
Hack Reactor Student <student@hackreactor.com>
2020-02-19 17:50:58 -0800

    do some change in search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,21 +4,54 @@ import VideoPlayer from '../../src/components/VideoPlayer.js';
 import exampleVideoData from '../../src/data/exampleVideoData.js';
 import Search from '../../src/components/Search.js';
 import searchYouTube from '../../src/lib/searchYouTube.js';
+import YOUTUBE_API_KEY from '../../src/config/youtube.js';
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      videoList: [],
+      videoList: exampleVideoData,
       currentVideo: exampleVideoData[0]
     };
     // searchYouTube()
   }
+  //get youtube videos by query
+  componentDidMount() {
+    this.getYoutubeVideos('cat');
+  }
 
+
+  getYoutubeVideos(query) {
+    console.log(this);
+    // console.log(searchYouTube);
+    var options = {
+
+      key: YOUTUBE_API_KEY,
+      query: query
+    };
+    searchYouTube(options, data => {
+      console.log('test2' + data);
+      this.setState({
+        videoList: data,
+        currentVideo: data[0]
+      });
+
+    });
+  }
+
+
+
+  //   searchYouTube(options, (videos) => {
+  //   //   this.setState({
+  //   //     videoListL: videos,
+  //   //     currentVideoL: videoList[0]
+  //   //   });
+  //   // });
+  // }
   // onCLick set currentVideo to clicked video
   handleClick(event, video) {
     event.preventDefault();
-    this.setState ({
+    this.setState({
       currentVideo: video
     });
   }
@@ -28,15 +61,15 @@ class App extends React.Component {
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em> <Search /></em></h5></div>
+            <div><h5><em> <Search handleChange={this.getYoutubeVideos.bind(this)} /></em></h5></div>
           </div>
         </nav>
         <div className="row">
           <div className="col-md-7">
-            <div><h5><em><VideoPlayer video={this.state.currentVideo}/></em></h5></div>
+            <div><h5><em><VideoPlayer video={this.state.currentVideo} /></em></h5></div>
           </div>
           <div className="col-md-5">
-            <div><h5><em><VideoList videos={this.state.videoList} handleClick={this.handleClick.bind(this)}/></em></h5></div>
+            <div><h5><em><VideoList videos={this.state.videoList} handleClick={this.handleClick.bind(this)} /></em></h5></div>
           </div>
         </div>
       </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,28 +1,28 @@
-class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+// class Search extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {value: ''};
+//     //this.handleSubmit = this.handleSubmit.bind(this);
+//   }
 
 
 
-  handleSubmit(event) {
-    console.log('just test');
-    event.preventDefault();
-  }
+// handleSubmit(event) {
+//   console.log('just test');
+//   event.preventDefault();
+// }
+var Search = ({handleChange}) => (
 
-  render() {
-    return (
-      <div className="search-bar form-inline">
-        <input className="form-control" type="text" />
-        <button className="btn hidden-sm-down" onClick={function(event) { handleSubmit(event); }}>
-          <span className="glyphicon glyphicon-search"></span>
-        </button>
-      </div>
-    );
-  }
-}
+  <div className="search-bar form-inline">
+    <input className="form-control" type="text"
+      onChange={(e) => handleChange(e.target.value)} />
+    <button className="btn hidden-sm-down" >
+      <span className="glyphicon glyphicon-search"></span>
+    </button>
+  </div>
+);
+
+
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default Search;
